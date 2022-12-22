8e51ac1345958b6101cb0bd8b32e25b51dceb1c4
Thomasdelecluse <81568452+Thomasdelecluse@users.noreply.github.com>
2022-12-22 22:46:02 +0100

    feat: add login

---
+++ site Saint Geroges/login.php
@@ -0,0 +1,8 @@
+<?php 
+
+$password = $_POST ['pass'];
+var_dump($password);
+
+
+
+?>
---
+++ site Saint Geroges/site.js
@@ -0,0 +1,43 @@
+
+
+window.addEventListener('load', () => {
+    const loader = document.querySelector('.loader');
+    console.log(loader.classList)
+    loader.classList.add('fonduu-outt');
+})
+
+document.querySelector('#contact-form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    e.target.elements.name.value = '';
+    e.target.elements.email.value = '';
+    e.target.elements.message.value = '';
+  });
+
+  console.clear();
+
+const loginBtn = document.getElementById('login');
+const signupBtn = document.getElementById('signup');
+
+loginBtn.addEventListener('click', (e) => {
+	let parent = e.target.parentNode.parentNode;
+	Array.from(e.target.parentNode.parentNode.classList).find((element) => {
+		if(element !== "slide-up") {
+			parent.classList.add('slide-up')
+		}else{
+			signupBtn.parentNode.classList.add('slide-up')
+			parent.classList.remove('slide-up')
+		}
+	});
+});
+
+signupBtn.addEventListener('click', (e) => {
+	let parent = e.target.parentNode;
+	Array.from(e.target.parentNode.classList).find((element) => {
+		if(element !== "slide-up") {
+			parent.classList.add('slide-up')
+		}else{
+			loginBtn.parentNode.parentNode.classList.add('slide-up')
+			parent.classList.remove('slide-up')
+		}
+	});
+});
